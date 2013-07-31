07c03566376801ff9fe3c85f6045480261af5d35
sankhurana <sankhurana@L-MAA-00800390.corp.ebay.com>
2013-07-31 16:53:00 +0530

    initial cell.java

--- src/Cell.java
+++ src/Cell.java
@@ -1,4 +1,25 @@
+import java.awt.Point;
+
 
 public class Cell {
-
+	private Point position;
+	private boolean isalive;
+	public Cell(Point position) {
+		this.position = position;
+		isalive = false;
+	}
+	
+	public void updateCell() {
+		if(isalive) isalive = false;
+		else isalive = true;
+	}
+	
+	public boolean isAlive() {
+		return (isalive == true);
+	}
+	
+	public String toString() {
+		if(isalive) return "o";
+		else return "";
+	}
 }
